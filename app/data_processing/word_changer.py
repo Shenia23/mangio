2fb1d72fe413e371f8fe28f5c07a1a4640604fbd
pablogarciastc <infopablogs@gmail.com>
2021-03-11 15:43:43 +0100

    added word_changer

--- app/data_processing/word_changer.py
+++ app/data_processing/word_changer.py
@@ -1,66 +1,86 @@
 from functools import reduce
 
 import pandas as pd
-import csv
-import re
+import pattern.es as es
 
 '''
 Código para cambiar palabras en el CSV de recetas, se introducen las equivalencias
 que deben ser cambiadas en la lista.
 
 '''
+input_path = '../../data/recetas.csv'
+output_path = '../../data/output.csv'
+words = [
+    #("papa", "patata"),
+    # ("jitomate", "tomate"),
+     ("jugo", "zumo")
+]
+
+#buscar también plurales
+def ingredients_to_plural(ingredients_raw):
+    # Add Pluralized bbc ingredients in order to catch ingredients like tomatoes, carrots, etc
+    ingredients_plural = []
+    for pair in words:
+        tuple = (es.pluralize(pair[0]),es.pluralize(pair[1]))
+        ingredients_plural.append(tuple)
+    ingredients_raw = ingredients_raw + ingredients_plural
+    return ingredients_raw
+
+def change_words(word1,word2,text):
+    text = ''.join([i for i in text]).replace(' '+word1+' ',' '+word2+' ')
+    text = ''.join([i for i in text]).replace(','+word1+' ',','+word2+' ')
+    text = ''.join([i for i in text]).replace(' '+word1+',',' '+word2+',')
+    text = ''.join([i for i in text]).replace(','+word1+',',','+word2+',')
+    text = ''.join([i for i in text]).replace(' ' + word1 + '.', ' ' + word2 + '.')
+    text = ''.join([i for i in text]).replace('.' + word1 + ' ', '.' + word2 + ' ')
+    text = ''.join([i for i in text]).replace(' '+word1+'|',' '+word2+'|')
+    text = ''.join([i for i in text]).replace('|'+word1+',','|'+word2+',')
+    text = ''.join([i for i in text]).replace('|'+word1+' ','|'+word2+' ')
+    text = ''.join([i for i in text]).replace('('+word1,'('+word2)
+    text = ''.join([i for i in text]).replace(' ' + word1 + '(', ' ' + word2 + '(')
+    text = ''.join([i for i in text]).replace(' ' + word1 + ')', ' ' + word2 + ')')
+    text = ''.join([i for i in text]).replace(',' + word1 + ')', ',' + word2 + ')')
+    text = ''.join([i for i in text]).replace(',' + word1 + '(', ',' + word2 + '(')
+    text = ''.join([i for i in text]).replace(' ' + word1 + '\n', ' ' + word2 + '\n')
+    text = ''.join([i for i in text]).replace(',' + word1 + '\n', ',' + word2 + '\n')
+    return text
 
 # Para no cambiar las palabras en las URLs
 def manageCol():
-    df = pd.read_csv('../../data/recetas.csv',sep='|')
+    df = pd.read_csv(input_path,sep='|')
     urls = df['Link_receta']
     del df['Link_receta']
-    df.to_csv('../../data/output.csv',sep='|',index=False)
+    df.to_csv(output_path,sep='|',index=False)
     return urls
 
 def saveCSV(text,urls):
-    x = open("../../data/output.csv", "w", encoding="utf8")
+    x = open(output_path, "w", encoding="utf8")
     x.writelines(text)
     x.close()
-    df = pd.read_csv('../../data/output.csv',sep='|')
+    df = pd.read_csv(output_path,sep='|')
     df['Link_receta'] = urls
-    df.to_csv('../../data/output.csv',sep='|',index=False)
+    df.to_csv(output_path,sep='|',index=False)
 
 #Llamar para observar si alguna se quedo igual, fuera de los links
 def viewer():
-    df = pd.read_csv('../../data/output.csv',sep='|')
+    df = pd.read_csv(output_path,sep='|')
     del df['Link_receta']
-    df.to_csv('../../data/output.csv',sep='|')
-
-def change_words(word1,word2,text):
-    text = ''.join([i for i in text]).replace(' '+word1,' '+word2)
-    text = ''.join([i for i in text]).replace(word1+' ',' '+word2+' ')
-    text = ''.join([i for i in text]).replace(','+word1,','+word2)
-    text = ''.join([i for i in text]).replace('('+word1,'('+word2)
-    text = ''.join([i for i in text]).replace(word1+',',word2+',')
-    text = ''.join([i for i in text]).replace(word1+'.',word2+'.')
-    text = ''.join([i for i in text]).replace(word1.capitalize(),word2.capitalize())
-    text = ''.join([i for i in text]).replace(word1.upper(),word2.upper())
-    return text
+    df.to_csv(output_path,sep='|')
 
 
+def main():
+    urls = manageCol()
+    text = open(output_path, "r", encoding="utf8")
+    all_words = ingredients_to_plural(words)
 
-urls = manageCol()
-text = open("../../data/output.csv", "r", encoding="utf8")
-
-words = [
-    ("papa", "patata"),
-    ("jitomate", "tomate"),
-    ("jugo", "zumo")
-]
-
-
+    for pair in all_words:
+        text = change_words(pair[0], pair[1],text)
+        text = change_words(pair[0].capitalize(), pair[1].capitalize(),text)
+        text = change_words(pair[0].upper(), pair[1].upper(),text)
 
 
+    saveCSV(text,urls)
+    #viewer()
 
-for pair in words:
-    text = change_words(pair[0], pair[1],text)
-
-saveCSV(text,urls)
-
-viewer()
+if __name__ == "__main__":
+    main()
