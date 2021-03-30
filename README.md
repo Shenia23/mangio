0a05ce607aa24467b03f4abfb795a4b594c75b3d
Shenia23 <ekuchumova@gradiant.org>
2021-03-30 14:11:19 +0200

    card animations + struct update

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Servicio de recomendación de una dieta a largo plazo con un objetivo saludable 
 
 ## Descripción
 
-Hygieia es un proyecto universitario centrado en proponer un plan de alimentación saludable a un usuario. 
+Mangio! es un proyecto universitario centrado en proponer un plan de alimentación saludable a un usuario. 
 
 Se basa en inputs sacados de la báscula Xiaomi My Body (BMI, masa muscular...) y las preferencias personales (objetivos, ingredientes habituales, feedback) para seleccionar un conjunto de recetas óptimas para una dieta equilibrada.
 
@@ -12,11 +12,20 @@ Se emplea un dataset de recetas españolas para las recomendaciones y se usa la 
 
 ## Estructura
 
-- Notebooks: libretas de jupyter auxiliares (scraping, preprocessing)
+Mangio!  
+├── run.py                      # flask app
+├── app                         # mangio app
+│   ├── data_processing         # data scripts for ingredient extraction...
+│   ├── user                    # user data and profiles
+│   └── recommender             # recommender logic
+├── data                        # app data as csv 
+├── notebooks                   # jupyter notebooks to test the application
+└── webapp                      # vue front-end
 
 ## Tecnologías
 
 Dependencias de python en requirements.txt
+`pip install -r requiriments.txt`
 
 ## Sources
 
--- webapp/frontend/src/components/Recomendacion.vue
+++ webapp/frontend/src/components/Recomendacion.vue
@@ -1,53 +1,57 @@
 <template>
-  <div class="recommendation-body">
-    <p></p>
-    <v-title class="headline mb-2"> Recomendación para Hoy </v-title>
-    <v-container>
-    <v-card
-      class="mx-auto"
-      max-width="550"
-      elevation=2
-      outlined
-      v-for="(comida, index) in comidas"
-      :key="index"
-    >  
-    <div class="d-flex flex-no-wrap justify-space-between">
-      <v-list-item three-line
-      >
-        <v-list-item-content>
-          <div class="overline mb-1">
-            {{ comida }}
-          </div>
-          <div v-for="(r, index) in rec"
-            :key="index"
-           >
-            <v-list-item v-if="r.tipo==comida" class="receta">
-                <v-list class="info_receta">
-                    <v-list-item-title class="body-1 mb-1" >
-                    <p class="text-left font-weight-bold">
-                            {{ r.nombre }}
-                        </p> 
-                    </v-list-item-title>
-                    <v-list-item-subtitle> 
-                        <p class="text-left caption">
-                            {{ r.calorias }} kcal
-                        </p> 
-                    </v-list-item-subtitle>
-                </v-list>
-                <v-divider></v-divider>
-                <v-list-item-avatar tile size="50" color="grey"></v-list-item-avatar>
-            </v-list-item>
-          </div>
-        </v-list-item-content>
+  <div class="row recommendation-body">
+    <v-col cols="12" md="8">
+    <!-- COL 1: cards de RECOMENDACIONES y RECETAS-->
+        <p></p>
+        <v-title class="headline mb-2"> Recomendación para Hoy </v-title>
+        <v-container>
+        <v-card
+        class="mx-auto"
+        max-width="550"
+        outlined
+        v-for="(comida, index) in comidas"
+        :key="index"
+        >  
+        <div class="d-flex flex-no-wrap justify-space-between">
+        <v-list-item three-line
+        >
+            <v-list-item-content>
+            <div class="overline mb-1">
+                {{ comida }}
+            </div>
+            <div v-for="(r, index) in rec"
+                :key="index"
+            >
+                <v-list-item v-if="r.tipo==comida" class="receta">
+                    <v-list class="info_receta">
+                        <v-list-item-title class="body-1 mb-1" >
+                        <p class="text-left font-weight-bold">
+                                {{ r.nombre }}
+                            </p> 
+                        </v-list-item-title>
+                        <v-list-item-subtitle> 
+                            <p class="text-left caption">
+                                {{ r.calorias }} kcal
+                            </p> 
+                        </v-list-item-subtitle>
+                    </v-list>
+                    <v-divider></v-divider>
+                    <v-list-item-avatar tile size="50" color="grey"></v-list-item-avatar>
+                </v-list-item>
+            </div>
+            </v-list-item-content>
 
-      </v-list-item>
-    </div>
-    </v-card>
-    </v-container>
+        </v-list-item>
+        </div>
+        </v-card>
+        </v-container>
+    </v-col>
 
-    <v-divider vertical></v-divider>
-
-    <p> Some stats here</p>
+    <v-col cols="12" md="4">
+    <!-- COL 2: STATS de las comidas + EXPLICACIÓN-->
+        <p> Stats </p>
+        <p> Explicación</p>
+    </v-col>
   </div>
 </template>
 
