2f4e0d8ad70e626585df300817f9dc641f28fb1b
Camilo Piñón Blanco <camipi2010@gmail.com>
2021-05-10 18:39:12 +0200

    Cambios en acceso usuarios

--- webapp/frontend/src/components/Users.vue
+++ webapp/frontend/src/components/Users.vue
@@ -1,79 +1,49 @@
 <template>
-  <v-container >
-      <div>
+  <v-container>
+    <div>
       <h1 class="title">Selección de usuario</h1>
     </div>
     <v-row align="center">
-      <v-item-group v-model="window" class="shrink mr-6" mandatory tag="v-flex">
-        <v-item v-for="n in users.length" :key="n" v-slot="{ active, toggle }">
-          <div>
-            <v-btn :input-value="active" icon @click="toggle">
-              <v-icon>mdi-record</v-icon>
-            </v-btn>
-          </div>
-        </v-item>
-      </v-item-group>
+      
+            
 
       <v-col>
         <v-window v-model="window" class="elevation-1" vertical>
-          <v-window-item v-for="n in users.length" :key="n">
-            <v-card flat>
-              <v-card-text>
-                <v-row class="mb-4" align="center">
-                  <v-avatar color="rgb(113, 192, 113)" class="mr-4"><v-icon>mdi-face</v-icon></v-avatar>
-                  <strong class="title"> Usuario {{ n }}: {{users[n-1].username}} </strong>
-                  <v-spacer></v-spacer>
-                  <v-btn icon @click="setUser(users[n - 1])">
-                    <v-icon>mdi-chevron-double-right</v-icon>
-                  </v-btn>
-                </v-row>
+          
 
-                <p>
-                  <b>Nombre</b> : {{users[n-1].name}}
-                </p>
-                <p>
-                  <b>Edad</b> : {{users[n-1].age}}
-                </p>
-                <p>
-                  <b>Sexo</b> : {{users[n-1].sex}}
-                </p>
-                <p>
-                  <b>Altura</b> : {{users[n-1].height}}
-                </p>
-
-                <p>
-                  <b>Peso</b> : {{users[n-1].weight}}
-                </p>
-                <p>
-                  <b>Tipo de cuerpo</b> : {{users[n-1].body_type}}
-                </p>
-
-                <p>
-                  <b>Nivel de actividad</b> : {{activity_types[users[n-1].activity_level-1]}}
-                </p>
-
-                <p>
-                  <b>Objetivo</b> : {{objectives[users[n-1].objective-1]}}
-                </p>
-
+          <v-list dense>
+            <v-list-item-group v-model="selectedItem" color="primary">
+              <v-list-item v-for="n in users.length" :key="n" @click="setUser(users[n - 1])">
                 
-              </v-card-text>
-            </v-card>
-          </v-window-item>
+                <v-list-item-content>
+                  <v-list-item-title v-text="users[n - 1].username"></v-list-item-title>
+                </v-list-item-content>
+
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
         </v-window>
       </v-col>
     </v-row>
+    <DialogPredetProfileSelected v-model="showPredetSelected" />
   </v-container>
 </template>
 
 <script>
+import DialogPredetProfileSelected from "./DialogPredetProfileSelected.vue";
+
+
 export default {
+  components: {
+    DialogPredetProfileSelected,
+  },
   props: {
-   users: Array
- },
+    users: Array,
+  },
   data: () => ({
     length: 3,
     window: 0,
+    showPredetSelected: false,
     activity_types: [
       "Sedentario (nada de ejercicio / trabajo de oficina)",
       "Ligeramente activo (ejercicio ligero de 1 a 3 días semanales)",
@@ -83,17 +53,16 @@ export default {
     ],
     objectives: ["Perder peso", "Mantener", "Ganar peso"],
   }),
-  methods:{ 
-
+  methods: {
     setUser(userData) {
       // FUNCION a introducir en el código de usuarios!
       console.log(userData);
+      this.showPredetSelected = true;
       this.$store.commit("setUserData", {
         userdata: userData,
       });
     },
-      
-  }
+  },
 };
 </script>
 
--- webapp/frontend/src/store/index.js
+++ webapp/frontend/src/store/index.js
@@ -97,10 +97,10 @@ export default new Vuex.Store({
          bmiText: state => {//anxo
             var auxbmi
             var bmi = state.user.userdata['bmi']
-            if (bmi < 18.5) return "esta por debajo de su peso ideal"
-            else if (bmi > 18.5 && bmi < 24.9) return "esta dentro de su peso ideal"
-            else if (bmi > 25 && bmi < 26.9) return "esta un poco por encima de su peso"
-            else if (bmi > 27 && bmi < 29.9) return "esta bastante por encima de su peso"
+            if (bmi < 18.5) return "está por debajo de su peso ideal"
+            else if (bmi > 18.5 && bmi < 24.9) return "está dentro de su peso ideal"
+            else if (bmi > 25 && bmi < 26.9) return "está un poco por encima de su peso"
+            else if (bmi > 27 && bmi < 29.9) return "está bastante por encima de su peso"
             else if (bmi > 30 && bmi < 34.9) return "está muy por encima de su peso"
             else if (bmi > 35 && bmi < 39.9) return "está muy por encima de su peso"
             else if (bmi > 40) return "está muy por encima de su peso"
