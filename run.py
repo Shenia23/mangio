5a5c335cbb0560cffb8c5d450a78b7483a776a11
Shenia23 <eugeku13@gmail.com>
2021-05-10 17:43:31 +0200

    v

--- run.py
+++ run.py
@@ -212,3 +212,6 @@ def catch_all(path):
     if app.debug:
         return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
+
+if __name__=='__main__':
+    app.run()
